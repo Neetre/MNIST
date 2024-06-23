1457d33615a0e67b106712727e53cfbd2118d157
19746 <mattiabraga2006@gmail.com>
2024-06-23 12:17:47 +0200

    PEP8 check done on gui

--- bin/MNIST_gui.py
+++ bin/MNIST_gui.py
@@ -1,21 +1,31 @@
 import gradio as gr
-import MNIST
+import mnist as MNIST
 
 
-def richiesta(image: str, compile: bool):
+def richiesta(image: str, compile_: bool):
+    """
+    The function that will be called when the button is pressed.
+
+    Args:
+        image (str): The path to the image
+        compile_ (bool): Compile the model
+
+    Returns:
+        str: The result of the inference
+    """
     MNIST.set_ic_gradio(False)
     device = MNIST.get_device()
-    if image != None:
+    if image is not None:
         try:
             image = MNIST.preprocess(image).to(device)
         except Exception as e:
             print(f"Error preprocessin the image: {e}")
 
         model = MNIST.Net()
-        model = MNIST.load_model(model, compile)
+        model = MNIST.load_model(model, compile_)
         result = MNIST.infer(model, device, image)
         result = MNIST.postprocess(result)
-        
+
     return str(result)
 
 
@@ -24,12 +34,12 @@ with gr.Blocks() as demo:
     gr.Markdown("### Drop the image you want to scan in the area below.")
     gr.Markdown("### The Image has to be black on white")
     image = gr.Image(type="filepath")
-    compile = gr.Checkbox(label="Compile the model")
+    compile_ = gr.Checkbox(label="Compile the model")
     result = gr.Textbox(label="Prediction of the NN")
-    
+
     btn = gr.Button("Run")
     btn.click(fn=richiesta,
-              inputs=[image, compile],
+              inputs=[image, compile_],
               outputs=result)
 
 
